1eec1565dc77347cfabfeba88951ae18aa482fa5
Ganesh Nehru <54563238+ganeshnehru@users.noreply.github.com>
2019-11-04 17:21:11 -0800

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -5,15 +5,73 @@ import javax.swing.*;
  */
 public class Tile extends JLabel
 {
+	//Defined variables.
     private Piece piece; // Null if empty
     private String tileColor;
-
-    public Tile(String tileColor)
+    public String WHITE = "white";
+    public String BLACK = "black";
+    
+    int x;
+    int y;
+    
+    String notation = "";
+    
+    boolean highlight;
+    
+    //Constructor
+    public Tile(boolean color, int x, int y)
     {
+    	this.x = x;
+    	this.y = y;
+    	this.highlight = false;
         this.piece = null;
-        this.tileColor = tileColor;
-    }
         
+        if(color == true)
+        {
+        	this.tileColor = WHITE;
+        }
+        else
+        {
+        	this.tileColor = BLACK;
+        }
+        
+        this.notation = Character.toString((char)(97+x));
+        this.notation += (char)(y+48+1);
+        
+    }
+    
+    //Returns x coordinate;
+    public int getX()
+    {
+    	return this.x;
+    }
+    
+    //Returns y coordinate.
+    public int getY()
+    {
+    	return this.y;
+    }
+    
+    //Returns color.
+    public String getColor()
+    {
+    	return this.tileColor;
+    }
+    
+    //Returns a removed piece.
+    public Piece removePiece()
+    {
+    	Piece p = this.piece;
+    	return p;
+    }
+    
+    //Sets a piece.
+    public void setPiece(Piece p)
+    {
+    	this.piece = p;
+    }
+    
+    //Checks if there is a piece present.
     public boolean isEmpty(){
         return this.piece == null;
     }
