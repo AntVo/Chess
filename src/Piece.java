18f6a67b166d9f6a584c8e4ee3aca7eb0adf3e9f
Arman Sandher <sandherarman@yahoo.com>
2019-11-27 19:52:06 -0800

    changes to piece and pawn for very basic valid spot checking

--- src/Piece.java
+++ src/Piece.java
@@ -12,14 +12,25 @@ public abstract class Piece extends JLabel
     Tile currentTile;
 
     public Piece(String imageLocation, String color, Tile tile) {
-        super();
+    	super();
         this.setIcon(new ImageIcon(imageLocation));
         this.color = color;
         this.currentTile = tile;
         this.currentTile.setPiece(this);
         this.currentTile.add(this);
     }
-
     
+    public boolean validSpot(ChessBoard board, int xStart, int yStart, int xEnd, int yEnd)
+    {
+    	if(xStart == xEnd && yStart == yEnd)
+    	{
+    		return false;
+    	}
+    	if(xEnd > 7 || xEnd < 0 || yEnd > 7 || yEnd < 0)
+    	{
+    		return false;
+    	}
+    	return true;
+    }
 }
 
